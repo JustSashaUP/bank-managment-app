809d698eed2172cfcbb30be5c89706c1c4c11e5c
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-04-18 14:38:38 +0300

    refactor project

---
+++ src/main/java/com/project/database/DBWorker.java
@@ -0,0 +1,28 @@
+package com.project.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBWorker {
+    private static final String HOST = "jdbc:mysql://localhost:3306/bank";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    private Connection connection;
+
+    public DBWorker()
+    {
+        try{
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+        }
+        catch(SQLException e)
+        {
+            System.err.println("database connection failed!");
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
---
+++ src/main/java/com/project/database/User.java
@@ -0,0 +1,103 @@
+package com.project.database;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Date birthDate;
+
+    public User() {}
+
+    public User(int id, String firstName, String lastName, String email, String phoneNumber, String password, String birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.birthDate = new Date(birthDate);
+    }
+
+    public User(String firstName, String lastName, String email, String phoneNumber, String password, String birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.birthDate = new Date(birthDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
