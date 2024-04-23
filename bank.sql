2b2752cdb21de48a3f17ddb7c83d5e265acc6ead
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-04-23 16:18:48 +0300

    add procedures

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2024 at 03:12 PM
+-- Generation Time: Apr 23, 2024 at 03:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
