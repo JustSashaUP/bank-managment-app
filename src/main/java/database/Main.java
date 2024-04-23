a583627157680b650c34810864f13a2a55ab965e
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-04-23 23:33:24 +0300

    update

---
+++ src/main/java/database/Main.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        DBWorker worker = new DBWorker();
+
+        String AllUsersQuery = "select * from client";
+        try {
+            Statement statement = worker.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(AllUsersQuery);
+
+            while(resultSet.next())
+            {
+                User user = new User();
+                user.setId(resultSet.getInt(1));
+                user.setFirstName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setPhoneNumber(resultSet.getString(4));
+                user.setEmail(resultSet.getString(5));
+                user.setBirthDate(resultSet.getDate(6));
+                user.setPassword(resultSet.getString(7));
+                System.out.println(user.toString());
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
