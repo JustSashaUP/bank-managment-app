abeb0c8cd4a74ddba3a66beec375bbc02f5a6ec1
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-04-27 01:08:45 +0300

    add cookiesServlet

---
+++ src/main/java/servlet/DeleteCookiesServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/deleteCookiesServlet")
+public class DeleteCookiesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie cookie = new Cookie("client_id", "");
+
+        cookie.setMaxAge(0);
+        resp.addCookie(cookie);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
