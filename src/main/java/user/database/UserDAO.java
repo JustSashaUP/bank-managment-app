f9f9734501273b247030cfb2b50951c1b664d24f
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-05-02 12:10:30 +0300

    update code, create getUser method

--- src/main/java/user/database/UserDAO.java
+++ src/main/java/user/database/UserDAO.java
@@ -9,8 +9,10 @@ import utils.fileutil.LoggerUtils;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 
 public class UserDAO {
+    private static User user;
     private static Logger logger;
     static
     {
@@ -47,7 +49,35 @@ public class UserDAO {
         return result;
     }
 
-    public boolean validate(User user)
+    public boolean loginValidate(User user)
+    {
+        DBWorker worker = new DBWorker();
+        logger.info("GET client data from database");
+        String SELECT_CLIENT_SQL = "select * from client where email = ? and password = ?";
+
+        boolean status = false;
+
+        try(PreparedStatement preparedStatement = worker.getConnection().prepareStatement(SELECT_CLIENT_SQL))
+        {
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getPassword());
+
+            logger.info("Statement: " + preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            status = resultSet.next();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace(System.err);
+            logger.error("GET client data from database ERROR!");
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("Error code: " + e.getErrorCode());
+            System.err.println("Message: " + e.getMessage());
+        }
+        return status;
+    }
+
+    public boolean registerValidate(User user)
     {
         DBWorker worker = new DBWorker();
         logger.info("GET client data from database");
@@ -77,7 +107,7 @@ public class UserDAO {
     public int getUserId(String userEmail)
     {
         DBWorker worker = new DBWorker();
-        User user = new User();
+        user = new User();
         logger.info("GET client_id data from database");
         String query = "call getClientIdByEmail(?)";
 
@@ -95,5 +125,35 @@ public class UserDAO {
             System.err.println("Message: " + e.getMessage());
         }
         return user.getId();
+    }
+
+    public User getUser(int id)
+    {
+        DBWorker worker = new DBWorker();
+        user = new User();
+        logger.info("GET client_id data from database");
+        String query = "call getClientData(?)";
+
+        try (PreparedStatement statement = worker.getConnection().prepareStatement(query)) {
+            statement.setString(1, String.valueOf(id));
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                user.setFirstName(resultSet.getString("first_name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPhoneNumber(resultSet.getString("phone_number"));
+                user.setBirthDate(resultSet.getString("birth_date"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            logger.error("GET client data from database ERROR!");
+            System.err.println("SQLState: " + e.getSQLState());
+            System.err.println("Error code: " + e.getErrorCode());
+            System.err.println("Message: " + e.getMessage());
+        } catch (ParseException e) {
+            logger.error("GET client data from database ERROR!");
+            System.err.println("Message: " + e.getMessage());
+        }
+        return user;
     }
 }
