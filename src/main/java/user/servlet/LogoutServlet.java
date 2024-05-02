4c7688c5d5253c7296692091223e18ad86a0188d
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-05-02 12:08:23 +0300

    delete session and redirect

---
+++ src/main/java/user/servlet/LogoutServlet.java
@@ -0,0 +1,36 @@
+package user.servlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import utils.fileutil.LoggerUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serial;
+
+@WebServlet("/logoutServlet")
+public class LogoutServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private static Logger logger;
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger = LogManager.getLogger(LogoutServlet.class);
+        LoggerUtils.setLogger(logger);
+        logger.info("Start LogoutServlet");
+        try(PrintWriter out = resp.getWriter()){
+            if(req.getSession().getAttribute("currentUserSession") != null) {
+                req.getSession().removeAttribute("currentUserSession");
+                resp.sendRedirect("index.jsp");
+            }else {
+                resp.sendRedirect("homePage.jsp");
+            }
+        }
+        logger.info("LogoutServlet finished");
+    }
+}
