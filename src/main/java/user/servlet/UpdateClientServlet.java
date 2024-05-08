e993daf9bde7732a64746d409b0be045bc00f40a
JustSashaUP <alexandr.savchenko04@gmail.com>
2024-05-08 21:14:08 +0300

    Update client servlet

---
+++ src/main/java/user/servlet/UpdateClientServlet.java
@@ -0,0 +1,45 @@
+package user.servlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import user.database.User;
+import user.database.UserDAO;
+import utils.fileutil.LoggerUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Serial;
+@WebServlet("/updateClientServlet")
+public class UpdateClientServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private static Logger logger;
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User();
+        HttpSession currentUserSession = req.getSession();
+        user = (User) currentUserSession.getAttribute("currentUserSession");
+        user.setEmail(req.getParameter("email"));
+        user.setPassword(req.getParameter("password"));
+        logger = LogManager.getLogger(LoginServlet.class);
+        LoggerUtils.setLogger(logger);
+
+        logger.info("start UpdateClientServlet🚀");
+
+        try
+        {
+            UserDAO.updateUser(user.getId(), user.getEmail(), user.getPassword());
+            currentUserSession.setAttribute("currentUserSession", user);
+        }
+        catch(Exception e)
+        {
+            logger.error("Login error❌: " + e.getMessage());
+        }
+        logger.info("UpdateClientServlet finished✅");
+    }
+}
